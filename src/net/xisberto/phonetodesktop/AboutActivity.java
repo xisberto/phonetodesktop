abfb1615f810066413abc178823b7d41c1513c75
Humberto Fraga <xisberto@gmail.com>
2012-07-27 00:05:06 -0300

    Added functionality to the buttons on AboutActivity.

--- src/net/xisberto/phonetodesktop/AboutActivity.java
+++ src/net/xisberto/phonetodesktop/AboutActivity.java
@@ -4,39 +4,56 @@ import android.content.Intent;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnLongClickListener;
+import android.view.View.OnClickListener;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 
-public class AboutActivity extends SherlockFragmentActivity {
+public class AboutActivity extends SherlockFragmentActivity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle arg0) {
 		super.onCreate(arg0);
 		setContentView(R.layout.layout_about);
 		
-		TextView text_name_version = (TextView)findViewById(R.id.text_name_version);
 		String versionName = "";
 		try {
 			versionName = " "+getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
 		} catch (NameNotFoundException e) {
 			versionName= "";
 		}
-		text_name_version.setText(getResources().getString(R.string.app_name)+versionName);
+		((TextView)findViewById(R.id.text_name_version)).setText(getResources().getString(R.string.app_name)+versionName);
 		
-		TextView text_about_message = (TextView)findViewById(R.id.text_about_message);
-		text_about_message.setOnLongClickListener(new OnLongClickListener() {
-			@Override
-			public boolean onLongClick(View v) {
-				Intent i = new Intent(Intent.ACTION_SEND);
-				i.setType("text/plain");
-				i.putExtra(Intent.EXTRA_TEXT, getResources().getString(R.string.link_about_message));
-				startActivity(Intent.createChooser(i, getResources().getString(R.string.send_to)));
-				return true;
-			}
-		});
+		findViewById(R.id.btn_share_about_message).setOnClickListener(this);
+		findViewById(R.id.btn_share_chrome).setOnClickListener(this);
+		findViewById(R.id.btn_share_google_java_api).setOnClickListener(this);
+		findViewById(R.id.btn_share_actionbarsherlock).setOnClickListener(this);
 		
+	}
+
+	@Override
+	public void onClick(View v) {
+		Intent i = new Intent(Intent.ACTION_SEND);
+		i.setType("text/plain");
+		switch (v.getId()) {
+		case R.id.btn_share_about_message:
+			i.putExtra(Intent.EXTRA_TEXT, getResources().getString(R.string.link_about_message));
+			break;
+		case R.id.btn_share_chrome:
+			i.putExtra(Intent.EXTRA_TEXT, getResources().getString(R.string.link_chrome_extension));
+			break;
+		case R.id.btn_share_google_java_api:
+			i.putExtra(Intent.EXTRA_TEXT, getResources().getString(R.string.link_google_java_api));
+			break;
+		case R.id.btn_share_actionbarsherlock:
+			i.putExtra(Intent.EXTRA_TEXT, getResources().getString(R.string.link_actionbarsherlock));
+			break;
+		default:
+			return;
+		}
+		Toast.makeText(getApplicationContext(), i.getStringExtra(Intent.EXTRA_TEXT), Toast.LENGTH_SHORT).show();
+		startActivity(Intent.createChooser(i, getResources().getString(R.string.send_to)));
 	}
 
 }
