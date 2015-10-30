35f760970a258a25880082628197837e272a07da
Humberto Fraga <xisberto@gmail.com>
2015-10-30 09:54:27 -0300

    Simplifying code, removing unused imports and implementations

--- phonetodesktop/src/main/java/net/xisberto/phonetodesktop/ui/MainActivity.java
+++ phonetodesktop/src/main/java/net/xisberto/phonetodesktop/ui/MainActivity.java
@@ -4,9 +4,9 @@
  * are made available under the terms of the GNU Public License v3.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/gpl.html
- * 
+ * <p/>
  * Contributors:
- *     Humberto Fraga <xisberto@gmail.com> - initial API and implementation
+ * Humberto Fraga <xisberto@gmail.com> - initial API and implementation
  ******************************************************************************/
 package net.xisberto.phonetodesktop.ui;
 
@@ -24,8 +24,6 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.Window;
 
 import com.google.android.gms.common.GoogleApiAvailability;
@@ -41,171 +39,165 @@ import net.xisberto.phonetodesktop.network.GoogleTasksSpiceService;
 import net.xisberto.phonetodesktop.network.TasksListRequest;
 
 public class MainActivity extends AppCompatActivity implements
-		OnClickListener, MainFragment.PhoneToDesktopAuthorization {
+        MainFragment.PhoneToDesktopAuthorization {
 
-	public static final int REQUEST_GOOGLE_PLAY_SERVICES = 0;
-	public static final int REQUEST_AUTHORIZATION = 1;
-	public static final int REQUEST_ACCOUNT_PICKER = 2;
+    public static final int REQUEST_GOOGLE_PLAY_SERVICES = 0;
+    public static final int REQUEST_AUTHORIZATION = 1;
+    public static final int REQUEST_ACCOUNT_PICKER = 2;
 
-	private static final String TAG_MAIN = "mainFragment";
+    private static final String TAG_MAIN = "mainFragment";
 
-	private GoogleAccountCredential credential;
-	public Preferences preferences;
+    private GoogleAccountCredential credential;
+    public Preferences preferences;
 
-	private MainFragment mainFragment;
-	private Fragment currentFragment;
+    private MainFragment mainFragment;
+    private Fragment currentFragment;
 
-	private boolean showWelcome;
+    private boolean showWelcome;
 
     private SpiceManager mSpiceManager = new SpiceManager(GoogleTasksSpiceService.class);
-	
-	private BroadcastReceiver receiver = new BroadcastReceiver() {
-		@Override
-		public void onReceive(Context context, Intent intent) {
-			if (intent.hasExtra(Utils.EXTRA_UPDATING)) {
-				updateMainLayout(intent.getBooleanExtra(Utils.EXTRA_UPDATING, false));
-			}
-		}
-	};
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		supportRequestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
-		setContentView(R.layout.activity_main);
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.hasExtra(Utils.EXTRA_UPDATING)) {
+                updateMainLayout(intent.getBooleanExtra(Utils.EXTRA_UPDATING, false));
+            }
+        }
+    };
 
-		preferences = Preferences.getInstance(this);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        setContentView(R.layout.activity_main);
 
-		credential = GoogleAccountCredential.usingOAuth2(this, Utils.scopes);
-		credential.setSelectedAccountName(preferences.loadAccountName());
+        preferences = Preferences.getInstance(this);
 
-		if (savedInstanceState == null) {
-			FragmentTransaction transaction = getSupportFragmentManager()
-					.beginTransaction();
-			if (preferences.loadAccountName() == null) {
-				showWelcome = true;
-				currentFragment = WelcomeFragment.newInstance();
-				transaction.replace(R.id.main_frame, currentFragment);
-			} else {
-				showWelcome = false;
-				mainFragment = MainFragment.newInstance();
-				transaction.replace(R.id.main_frame, mainFragment, TAG_MAIN);
-			}
-			transaction.commit();
-		} else {
-			mainFragment = (MainFragment) getSupportFragmentManager().findFragmentByTag(
-					TAG_MAIN);
-		}
+        credential = GoogleAccountCredential.usingOAuth2(this, Utils.scopes);
+        credential.setSelectedAccountName(preferences.loadAccountName());
 
-		String action = getIntent().getAction();
-		if (action != null && action.equals(Utils.ACTION_AUTHENTICATE)) {
-			updateMainLayout(true);
-			startAuthorization();
-		}
-	}
+        if (savedInstanceState == null) {
+            FragmentTransaction transaction = getSupportFragmentManager()
+                    .beginTransaction();
+            if (preferences.loadAccountName() == null) {
+                showWelcome = true;
+                currentFragment = WelcomeFragment.newInstance();
+                transaction.replace(R.id.main_frame, currentFragment);
+            } else {
+                showWelcome = false;
+                mainFragment = MainFragment.newInstance();
+                transaction.replace(R.id.main_frame, mainFragment, TAG_MAIN);
+            }
+            transaction.commit();
+        } else {
+            mainFragment = (MainFragment) getSupportFragmentManager().findFragmentByTag(
+                    TAG_MAIN);
+        }
 
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
+        String action = getIntent().getAction();
+        if (action != null && action.equals(Utils.ACTION_AUTHENTICATE)) {
+            updateMainLayout(true);
+            startAuthorization();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         mSpiceManager.start(this);
 
-		switch (requestCode) {
-		case REQUEST_ACCOUNT_PICKER:
-			Utils.log("Result from Account Picker");
-			if (resultCode == RESULT_OK && data != null
-					&& data.hasExtra(AccountManager.KEY_ACCOUNT_NAME)) {
-				String accountName = data.getExtras().getString(
-						AccountManager.KEY_ACCOUNT_NAME);
-				Utils.log("Saving account " + accountName);
-				if (accountName != null) {
-					if (currentFragment instanceof WelcomeFragment) {
-						mainFragment = (MainFragment) getSupportFragmentManager()
-								.findFragmentByTag(TAG_MAIN);
-						if (mainFragment == null) {
-							mainFragment = MainFragment.newInstance();
-						}
-						updateMainLayout(true);
-						getSupportFragmentManager().beginTransaction()
-								.replace(R.id.main_frame, mainFragment)
-								.setTransition(FragmentTransaction.TRANSIT_NONE)
-								.commit();
-					}
-					credential.setSelectedAccountName(accountName);
-					preferences.saveAccountName(accountName);
-					// If PhoneToDesktop hasn't been authorized by the user
-					// this will lead to an UserRecoverableAuthIOException
-					// that will generate an onActivityResult for
-					// REQUEST_AUTHENTICATION
-                    asyncRequestLists();
-				}
-			} else {
-				// User cancelled, or any other error during authorization
-				// updateMainLayout(false);
-			}
-			break;
+        switch (requestCode) {
+            case REQUEST_ACCOUNT_PICKER:
+                Utils.log("Result from Account Picker");
+                if (resultCode == RESULT_OK && data != null
+                        && data.hasExtra(AccountManager.KEY_ACCOUNT_NAME)) {
+                    String accountName = data.getExtras().getString(
+                            AccountManager.KEY_ACCOUNT_NAME);
+                    Utils.log("Saving account " + accountName);
+                    if (accountName != null) {
+                        if (currentFragment instanceof WelcomeFragment) {
+                            mainFragment = (MainFragment) getSupportFragmentManager()
+                                    .findFragmentByTag(TAG_MAIN);
+                            if (mainFragment == null) {
+                                mainFragment = MainFragment.newInstance();
+                            }
+                            updateMainLayout(true);
+                            getSupportFragmentManager().beginTransaction()
+                                    .replace(R.id.main_frame, mainFragment)
+                                    .setTransition(FragmentTransaction.TRANSIT_NONE)
+                                    .commit();
+                        }
+                        credential.setSelectedAccountName(accountName);
+                        preferences.saveAccountName(accountName);
+                        // If PhoneToDesktop hasn't been authorized by the user
+                        // this will lead to an UserRecoverableAuthIOException
+                        // that will generate an onActivityResult for
+                        // REQUEST_AUTHENTICATION
+                        saveListId();
+                    }
+                // else
+                    // User cancelled, or any other error during authorization
+                    // updateMainLayout(false);
+                }
+                break;
 
-		case REQUEST_GOOGLE_PLAY_SERVICES:
-			Utils.log("Result from Play Services error");
-			startAuthorization();
-			break;
-		case REQUEST_AUTHORIZATION:
-			Utils.log("Result from Authorization");
-			if (resultCode == RESULT_OK) {
-                asyncRequestLists();
-			} else {
-				updateMainLayout(false);
-			}
-			break;
+            case REQUEST_GOOGLE_PLAY_SERVICES:
+                Utils.log("Result from Play Services error");
+                startAuthorization();
+                break;
+            case REQUEST_AUTHORIZATION:
+                Utils.log("Result from Authorization");
+                if (resultCode == RESULT_OK) {
+                    saveListId();
+                } else {
+                    updateMainLayout(false);
+                }
+                break;
 
-		default:
-			break;
-		}
-	}
+            default:
+                break;
+        }
+    }
 
-	@Override
-	protected void onStart() {
-		super.onStart();
-		LocalBroadcastManager.getInstance(this).registerReceiver(receiver,
+    @Override
+    protected void onStart() {
+        super.onStart();
+        LocalBroadcastManager.getInstance(this).registerReceiver(receiver,
                 new IntentFilter(Utils.ACTION_AUTHENTICATE));
-	}
+    }
 
-	@Override
-	protected void onStop() {
-		super.onStop();
-		LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver);
+    @Override
+    protected void onStop() {
+        super.onStop();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver);
         if (mSpiceManager.isStarted()) {
             mSpiceManager.shouldStop();
         }
     }
 
-	@Override
-	public void onClick(View v) {
-
-	}
-
-	@Override
-	public void startAuthorization() {
-		if (checkGooglePlayServicesAvailable()) {
-			// ask user to choose account
-			startActivityForResult(credential.newChooseAccountIntent(),
+    @Override
+    public void startAuthorization() {
+        if (checkGooglePlayServicesAvailable()) {
+            // ask user to choose account
+            startActivityForResult(credential.newChooseAccountIntent(),
                     REQUEST_ACCOUNT_PICKER);
-		}
-	}
+        }
+    }
 
-	/** Check that Google Play services APK is installed and up to date. */
-	public boolean checkGooglePlayServicesAvailable() {
+    /** Check that Google Play services APK is installed and up to date. */
+    public boolean checkGooglePlayServicesAvailable() {
         GoogleApiAvailability googleApiAvailability = GoogleApiAvailability.getInstance();
-		final int connectionStatusCode = googleApiAvailability
+        final int connectionStatusCode = googleApiAvailability
                 .isGooglePlayServicesAvailable(this);
-		if (googleApiAvailability.isUserResolvableError(connectionStatusCode)) {
+        if (googleApiAvailability.isUserResolvableError(connectionStatusCode)) {
             showGooglePlayServicesAvailabilityErrorDialog(connectionStatusCode);
-			return false;
-		}
-		return true;
-	}
+            return false;
+        }
+        return true;
+    }
 
-	public void showGooglePlayServicesAvailabilityErrorDialog(
-			final int connectionStatusCode) {
+    public void showGooglePlayServicesAvailabilityErrorDialog(final int connectionStatusCode) {
         runOnUiThread(new Runnable() {
             public void run() {
                 Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(
@@ -214,19 +206,22 @@ public class MainActivity extends AppCompatActivity implements
                 dialog.show();
             }
         });
-	}
+    }
 
-	private void updateMainLayout(boolean updating) {
-		Utils.log("updating layout " + updating);
-		if (mainFragment != null) {
-			mainFragment.setUpdating(updating);
-			if (mainFragment.isVisible()) {
-				mainFragment.updateMainLayout();
-			}
-		}
-	}
+    private void updateMainLayout(boolean updating) {
+        Utils.log("updating layout " + updating);
+        if (mainFragment != null) {
+            mainFragment.setUpdating(updating);
+            if (mainFragment.isVisible()) {
+                mainFragment.updateMainLayout();
+            }
+        }
+    }
 
-	private void asyncRequestLists() {
+    /**
+     * Retrieves the list id to use. Creates a new list if no list named "PhoneToDesktop" is found.
+     */
+    private void saveListId() {
         if (!mSpiceManager.isStarted()) {
             mSpiceManager.start(this);
         }
@@ -251,36 +246,36 @@ public class MainActivity extends AppCompatActivity implements
         mSpiceManager.execute(request, listRequestListener);
     }
 
-	public static class RetryDialog extends DialogFragment implements
-			DialogInterface.OnClickListener {
-		private MainActivity activity;
+    public static class RetryDialog extends DialogFragment implements
+            DialogInterface.OnClickListener {
+        private MainActivity activity;
 
-		public static RetryDialog newInstance(MainActivity act) {
-			RetryDialog dialog = new RetryDialog();
-			dialog.activity = act;
-			return dialog;
-		}
+        public static RetryDialog newInstance(MainActivity act) {
+            RetryDialog dialog = new RetryDialog();
+            dialog.activity = act;
+            return dialog;
+        }
 
-		@Override
-		public Dialog onCreateDialog(Bundle savedInstanceState) {
-			AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(activity)
-					.setTitle(R.string.app_name).setMessage(R.string.txt_retry)
-					.setPositiveButton(android.R.string.yes, this)
-					.setNegativeButton(android.R.string.no, this);
-			return dialogBuilder.create();
-		}
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(activity)
+                    .setTitle(R.string.app_name).setMessage(R.string.txt_retry)
+                    .setPositiveButton(android.R.string.yes, this)
+                    .setNegativeButton(android.R.string.no, this);
+            return dialogBuilder.create();
+        }
 
-		@Override
-		public void onClick(DialogInterface dialog, int which) {
-			switch (which) {
-			case DialogInterface.BUTTON_POSITIVE:
-				activity.asyncRequestLists();
-				dialog.dismiss();
-			case DialogInterface.BUTTON_NEGATIVE:
-				activity.updateMainLayout(false);
-				dialog.dismiss();
-			}
-		}
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            switch (which) {
+                case DialogInterface.BUTTON_POSITIVE:
+                    activity.saveListId();
+                    dialog.dismiss();
+                case DialogInterface.BUTTON_NEGATIVE:
+                    activity.updateMainLayout(false);
+                    dialog.dismiss();
+            }
+        }
 
-	}
+    }
 }
--- phonetodesktop/src/main/java/net/xisberto/phonetodesktop/ui/MainFragment.java
+++ phonetodesktop/src/main/java/net/xisberto/phonetodesktop/ui/MainFragment.java
@@ -1,6 +1,6 @@
 package net.xisberto.phonetodesktop.ui;
 
-import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -11,7 +11,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-import net.xisberto.phonetodesktop.BuildConfig;
 import net.xisberto.phonetodesktop.Preferences;
 import net.xisberto.phonetodesktop.R;
 import net.xisberto.phonetodesktop.Utils;
@@ -22,149 +21,147 @@ import net.xisberto.phonetodesktop.Utils;
  * {@link MainFragment.PhoneToDesktopAuthorization} interface to handle
  * interaction events. Use the {@link MainFragment#newInstance} factory method
  * to create an instance of this fragment.
- * 
  */
 public class MainFragment extends Fragment implements OnClickListener {
 
-	private View mView;
+    private View mView;
 
-	private PhoneToDesktopAuthorization mListener;
+    private PhoneToDesktopAuthorization mListener;
 
-	private boolean mUpdating;
+    private boolean mUpdating;
 
-	public static MainFragment newInstance() {
-		MainFragment fragment = new MainFragment();
-		Bundle args = new Bundle();
-		fragment.setArguments(args);
-		fragment.setUpdating(false);
-		return fragment;
-	}
+    public static MainFragment newInstance() {
+        MainFragment fragment = new MainFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        fragment.setUpdating(false);
+        return fragment;
+    }
 
-	public MainFragment() {
-		// Required empty public constructor
-	}
+    public MainFragment() {
+        // Required empty public constructor
+    }
 
-	@Override
-	public void onAttach(Activity activity) {
-		super.onAttach(activity);
-		try {
-			mListener = (PhoneToDesktopAuthorization) activity;
-		} catch (ClassCastException e) {
-			throw new ClassCastException(activity.toString()
-					+ " must implement OnFragmentInteractionListener");
-		}
-	}
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            mListener = (PhoneToDesktopAuthorization) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		mView = inflater.inflate(R.layout.fragment_main, container, false);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.fragment_main, container, false);
 
-		mView.findViewById(R.id.btn_authorize).setOnClickListener(this);
-		mView.findViewById(R.id.btn_link_list).setOnClickListener(this);
-		mView.findViewById(R.id.btn_wait_list).setOnClickListener(this);
-		mView.findViewById(R.id.btn_how_it_works).setOnClickListener(this);
-		mView.findViewById(R.id.btn_preferences).setOnClickListener(this);
-		mView.findViewById(R.id.btn_about).setOnClickListener(this);
+        mView.findViewById(R.id.btn_authorize).setOnClickListener(this);
+        mView.findViewById(R.id.btn_link_list).setOnClickListener(this);
+        mView.findViewById(R.id.btn_wait_list).setOnClickListener(this);
+        mView.findViewById(R.id.btn_how_it_works).setOnClickListener(this);
+        mView.findViewById(R.id.btn_preferences).setOnClickListener(this);
+        mView.findViewById(R.id.btn_about).setOnClickListener(this);
 
-		return mView;
-	}
+        return mView;
+    }
 
-	@Override
-	public void onActivityCreated(Bundle savedInstanceState) {
-		super.onActivityCreated(savedInstanceState);
-	}
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
 
-	@Override
-	public void onStart() {
-		super.onStart();
-	}
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
 
-	@Override
-	public void onResume() {
-		super.onResume();
-		updateMainLayout();
-	}
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateMainLayout();
+    }
 
-	@Override
-	public void onDetach() {
-		super.onDetach();
-		mListener = null;
-	}
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
 
-	@Override
-	public void onClick(View v) {
-		switch (v.getId()) {
-		case R.id.btn_how_it_works:
-			startActivity(new Intent(getActivity(), TutorialActivity.class));
-			break;
-		case R.id.btn_about:
-			startActivity(new Intent(getActivity(), AboutActivity.class));
-			break;
-		case R.id.btn_link_list:
-			startActivity(new Intent(getActivity(), LinkListActivity.class));
-			break;
-		case R.id.btn_wait_list:
-			startActivity(new Intent(getActivity(), WaitListActivity.class));
-			break;
-		case R.id.btn_preferences:
-			startActivity(new Intent(getActivity(), PreferencesActivity.class));
-			break;
-		case R.id.btn_authorize:
-			setUpdating(true);
-			updateMainLayout();
-			mListener.startAuthorization();
-		}
-	}
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_how_it_works:
+                startActivity(new Intent(getActivity(), TutorialActivity.class));
+                break;
+            case R.id.btn_about:
+                startActivity(new Intent(getActivity(), AboutActivity.class));
+                break;
+            case R.id.btn_link_list:
+                startActivity(new Intent(getActivity(), LinkListActivity.class));
+                break;
+            case R.id.btn_wait_list:
+                startActivity(new Intent(getActivity(), WaitListActivity.class));
+                break;
+            case R.id.btn_preferences:
+                startActivity(new Intent(getActivity(), PreferencesActivity.class));
+                break;
+            case R.id.btn_authorize:
+                setUpdating(true);
+                updateMainLayout();
+                mListener.startAuthorization();
+        }
+    }
 
-	public void setUpdating(boolean updating) {
-		Utils.log("changing updating to " + updating);
-		mUpdating = updating;
-	}
-	
-	public void updateMainLayout() {
-		Utils.log("fragment updating "+mUpdating);
-		Button btn_authorize = (Button) mView.findViewById(R.id.btn_authorize);
-		TextView txt_authorize = (TextView) mView.findViewById(R.id.txt_authorize);
+    public void setUpdating(boolean updating) {
+        Utils.log("changing updating to " + updating);
+        mUpdating = updating;
+    }
 
-		getActivity().setProgressBarIndeterminateVisibility(mUpdating);
-		btn_authorize.setEnabled(!mUpdating);
+    public void updateMainLayout() {
+        Utils.log("fragment updating " + mUpdating);
+        Button btn_authorize = (Button) mView.findViewById(R.id.btn_authorize);
+        TextView txt_authorize = (TextView) mView.findViewById(R.id.txt_authorize);
 
-		if (mUpdating) {
-			txt_authorize.setText(R.string.txt_waiting_authorization);
-		} else {
-			Preferences prefs = Preferences.getInstance(getActivity());
-			String account_name = prefs.loadAccountName();
-			if (account_name != null) {
-				txt_authorize.setText(getString(R.string.txt_authorized_to) + " "
-						+ account_name);
-				btn_authorize.setText(R.string.btn_authorize_other);
+        getActivity().setProgressBarIndeterminateVisibility(mUpdating);
+        btn_authorize.setEnabled(!mUpdating);
+
+        if (mUpdating) {
+            txt_authorize.setText(R.string.txt_waiting_authorization);
+        } else {
+            Preferences prefs = Preferences.getInstance(getActivity());
+            String account_name = prefs.loadAccountName();
+            if (account_name != null) {
+                txt_authorize.setText(getString(R.string.txt_authorized_to, account_name));
+                btn_authorize.setText(R.string.btn_authorize_other);
 //				if (BuildConfig.DEBUG) {
 //                    getActivity().getActionBar().setSubtitle(
 //							"List: " + prefs.loadListId());
 //				}
-			} else {
-				txt_authorize.setText(R.string.txt_authorize);
-			}
-		}
-	}
+            } else {
+                txt_authorize.setText(R.string.txt_authorize);
+            }
+        }
+    }
 
-	/**
-	 * This interface must be implemented by activities that contain this
-	 * fragment to allow an interaction in this fragment to be communicated to
-	 * the activity and potentially other fragments contained in that activity.
-	 * <p>
-	 * See the Android Training lesson <a href=
-	 * "http://developer.android.com/training/basics/fragments/communicating.html"
-	 * >Communicating with Other Fragments</a> for more information.
-	 */
-	public interface PhoneToDesktopAuthorization {
-		public void startAuthorization();
-	}
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated to
+     * the activity and potentially other fragments contained in that activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface PhoneToDesktopAuthorization {
+        void startAuthorization();
+    }
 
 }
